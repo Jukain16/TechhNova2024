9481d3da5d8e76dc0b13ad5f28cd17070af0cea6
Sarah <sarah@localhost.localdomain>
2024-09-28 16:12:55 -0400

    Initialized database

--- working-name/app.py
+++ working-name/app.py
@@ -6,6 +6,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
             SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path, 'working-name.json'),
             )
     if test_config is None:
         app.config.from_mapping(test_config)
@@ -18,4 +19,6 @@ def create_app(test_config=None):
     @app.route('/')
     def hello():
         return 'Hello, World!'
+    from . import db
+    db.init_app(app)
     return app
---
+++ working-name/db.py
@@ -0,0 +1,16 @@
+from tinydb import TinyDB, Query
+db = TinyDB('')
+
+def get_db():
+    if 'db' not in g:
+        g.db = json.connect(
+                current_app.config['DATABASE'],
+                detect_types=json.PARSE_DECLTYPES
+                )
+        g.db.row_factory = json.Row
+    return g.db
+def close_db(e=None):
+    db = g.pop('db', None)
+    
+    if db is not None:
+        db.close()
